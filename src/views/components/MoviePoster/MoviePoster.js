197fa2a7adbae2143a78fa8b5a12acc79fa51901
Miguel Zabala F <mzabala@serempre.com>
2021-09-06 13:47:35 -0500

    Add touchable opacity in moviePosterComponent

--- src/views/components/MoviePoster/MoviePoster.js
+++ src/views/components/MoviePoster/MoviePoster.js
@@ -1,38 +1,50 @@
 import React from 'react';
-import { View, Text, Image, StyleSheet } from 'react-native';
+import { View, Image, StyleSheet, TouchableOpacity } from 'react-native';
+import useControllers from '../../../controllers';
 
 const MoviePoster = ({ movie, width = 250, height = 400 }) => {
 
   const posterUrl = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
 
+  const { useGeneralHooks } = useControllers();
+  const { useNavigation } = useGeneralHooks();
+  const { navigateTo } = useNavigation();
+
   return (
-    <View style={{ width, height, marginHorizontal: 7 }}>
+    <TouchableOpacity
+      style={{ width, height, marginHorizontal: 7, ...styles.moviePosterContainer }}
+      activeOpacity={0.75}
+      onPress={() => navigateTo('DetailMovieScreen', movie)}
+    >
       <View style={styles.moviePosterImageContainer}>
         <Image
           source={{ uri: posterUrl }}
           style={styles.moviePosterImage}
         />
       </View>
-    </View>
+    </TouchableOpacity>
   );
 };
 
 export default MoviePoster;
 
 const styles = StyleSheet.create({
-  moviePosterImageContainer: {
-    flex: 1,
-    borderRadius: 18,
+  moviePosterContainer: {
     shadowColor: "#000",
     shadowOffset: {
       width: 0,
       height: 5,
     },
-    shadowOpacity: 0.15,
-    shadowRadius: 5.84,
+    shadowOpacity: 0.35,
+    shadowRadius: 5,
     elevation: 5,
   },
+  moviePosterImageContainer: {
+    flex: 1,
+    borderRadius: 18,
+  },
   moviePosterImage: {
+    backgroundColor: '#C3C3C3',
     flex: 1,
     borderRadius: 18
   }
